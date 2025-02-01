ee081a2f6e52c780221dcfcc8b15fae10df651e3
Ibrahim <benlakhal19@gmail.com>
2025-02-02 00:23:20 +0100

    Adding Mailtrap, for testing only

--- pkg/notification/model.go
+++ pkg/notification/model.go
@@ -2,14 +2,17 @@ package notification
 
 // Request defines the payload structure for sending notifications
 type Request struct {
-	To          string            `json:"to"` // Recipient (email, phone, etc.)
-	Subject     string            `json:"subject"`
-	DynamicData map[string]string `json:"dynamicData"` // Dynamic content for the template
-	test        string            `json:"subject"`
+	Method      string
+	URL         string
+	Body        []byte
+	Header      map[string]string
+	To          string
+	DynamicData map[string]string
 }
 
 // Response represents the response from the notification service
 type Response struct {
+	Header  string
 	ID      string
 	Message string `json:"message"` // Success message
 }
